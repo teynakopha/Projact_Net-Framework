19b3cd0928995ae1c946ceca5ca118182236efbe
SattayaLaptop <teynakopha@gmail.com>
2018-01-24 11:34:48 +0700

    Add project files.

---
+++ TAPB/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace TAPB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox_ServerIP = new System.Windows.Forms.TextBox();
+            this.textBox_User = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.button_testconnect = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(980, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(455, 235);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(413, 325);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(822, 127);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(175, 102);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(874, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "query";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox_ServerIP
+            // 
+            this.textBox_ServerIP.Location = new System.Drawing.Point(82, 23);
+            this.textBox_ServerIP.Name = "textBox_ServerIP";
+            this.textBox_ServerIP.Size = new System.Drawing.Size(209, 22);
+            this.textBox_ServerIP.TabIndex = 4;
+            // 
+            // textBox_User
+            // 
+            this.textBox_User.Location = new System.Drawing.Point(372, 24);
+            this.textBox_User.Name = "textBox_User";
+            this.textBox_User.Size = new System.Drawing.Size(86, 22);
+            this.textBox_User.TabIndex = 5;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(539, 27);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(140, 22);
+            this.textBox_Password.TabIndex = 6;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(788, 27);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(75, 22);
+            this.textBox_port.TabIndex = 7;
+            // 
+            // button_testconnect
+            // 
+            this.button_testconnect.Location = new System.Drawing.Point(879, 21);
+            this.button_testconnect.Name = "button_testconnect";
+            this.button_testconnect.Size = new System.Drawing.Size(116, 61);
+            this.button_testconnect.TabIndex = 8;
+            this.button_testconnect.Text = "Connect";
+            this.button_testconnect.UseVisualStyleBackColor = true;
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(252, 231);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(92, 71);
+            this.button_save.TabIndex = 9;
+            this.button_save.Text = "save";
+            this.button_save.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_ServerIP);
+            this.groupBox1.Controls.Add(this.textBox_User);
+            this.groupBox1.Controls.Add(this.button_testconnect);
+            this.groupBox1.Controls.Add(this.textBox_Password);
+            this.groupBox1.Controls.Add(this.textBox_port);
+            this.groupBox1.Location = new System.Drawing.Point(12, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1107, 119);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Config vCenter server Database";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(691, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Port(optional)";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(464, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Server IP ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.button_save);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 138);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(411, 366);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Scheduler Config delete log";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 160);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Daily Run";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "HH:mm";
+            this.dateTimePicker1.Location = new System.Drawing.Point(155, 155);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(188, 22);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(352, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "วัน";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.comboBox2.Location = new System.Drawing.Point(154, 101);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(189, 24);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ลบ log ที่มีอายุเกิน";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Delete log from User";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(155, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(741, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "buttonTest";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 616);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Design By Sattaya";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox_ServerIP;
+        private System.Windows.Forms.TextBox textBox_User;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Button button_testconnect;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TAPB/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using System.Data.SQLite;
+namespace TAPB
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlCommand deleteCMD;
+        NpgsqlCommand deleteCMD2;
+        string vpx_delete;
+        string vpxarg_delete;
+        List<int> eventid = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitiaForm();
+        }
+        public void InitiaForm()
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Time;
+            dateTimePicker1.ShowUpDown = true;
+        }
+        public void connectDB()
+        {
+ 
+            try
+            {
+                NpgsqlConnection conn = new NpgsqlConnection("Server=10.10.10.222;User Id=postgres; " +
+                "Password=QK5F#&2IH}dwmkjN;Database=VCDB;");
+                richTextBox1.Text = "success";
+                conn.Open();
+                
+                // Define a query returning a single row result set
+                NpgsqlCommand command = new NpgsqlCommand("select event_id from vpx_event where username = 'root';", conn);
+                MessageBox.Show(richTextBox2.Text);
+                NpgsqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    eventid.Add(Convert.ToInt16(dr[0]));
+                   
+                }
+                conn.Close();
+            }
+           catch (Exception e)
+            {
+                richTextBox1.Text = e.ToString();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connectDB();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            d();
+        }
+        private void d()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=10.10.10.222;User Id=postgres; " +
+"Password=QK5F#&2IH}dwmkjN;Database=VCDB;");
+            conn.Open();
+            foreach (int a in eventid)
+            {
+                richTextBox1.Text += " delete id :" + a + "\n";
+                vpx_delete = string.Format("delete from vpx_event where event_id = {0}", a);
+                NpgsqlCommand command = new NpgsqlCommand(vpx_delete, conn);
+                command.ExecuteNonQuery();
+                command.CommandText = string.Format("delete from vpx_event_arg where event_id = {0}", a);
+                command.ExecuteNonQuery();
+            }
+
+            conn.Close();
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Console.Write("df");
+        }
+
+        // db config sqlite 
+        public void saveconfig()
+        {
+            
+            SQLiteConnection.CreateFile("Config.db");
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=Config.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveconfig();
+        }
+    }
+}
