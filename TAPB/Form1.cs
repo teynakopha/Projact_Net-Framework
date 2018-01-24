315809553243b5653f4500ccc143ea6d9b0b49ad
SattayaLaptop <teynakopha@gmail.com>
2018-01-24 12:33:44 +0700

    create class SQLite

--- TAPB/Form1.cs
+++ TAPB/Form1.cs
@@ -34,14 +34,14 @@ namespace TAPB
         }
         public void connectDB()
         {
- 
+
             try
             {
                 NpgsqlConnection conn = new NpgsqlConnection("Server=10.10.10.222;User Id=postgres; " +
                 "Password=QK5F#&2IH}dwmkjN;Database=VCDB;");
                 richTextBox1.Text = "success";
                 conn.Open();
-                
+
                 // Define a query returning a single row result set
                 NpgsqlCommand command = new NpgsqlCommand("select event_id from vpx_event where username = 'root';", conn);
                 MessageBox.Show(richTextBox2.Text);
@@ -49,15 +49,15 @@ namespace TAPB
                 while (dr.Read())
                 {
                     eventid.Add(Convert.ToInt16(dr[0]));
-                   
+
                 }
                 conn.Close();
             }
-           catch (Exception e)
+            catch (Exception e)
             {
                 richTextBox1.Text = e.ToString();
             }
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -82,10 +82,11 @@ namespace TAPB
                 command.ExecuteNonQuery();
                 command.CommandText = string.Format("delete from vpx_event_arg where event_id = {0}", a);
                 command.ExecuteNonQuery();
+
             }
 
             conn.Close();
-            
+
         }
 
         private void label4_Click(object sender, EventArgs e)
@@ -96,7 +97,7 @@ namespace TAPB
         // db config sqlite 
         public void saveconfig()
         {
-            
+
             SQLiteConnection.CreateFile("Config.db");
             SQLiteConnection m_dbConnection;
             m_dbConnection = new SQLiteConnection("Data Source=Config.sqlite;Version=3;");
@@ -106,6 +107,37 @@ namespace TAPB
         private void button3_Click(object sender, EventArgs e)
         {
             saveconfig();
+        }
+    }
+    public class SQLiteOperation
+    {
+        public SQLiteOperation()
+        {
+            if(checkFilesExit())
+            {
+                connectDB();
+            }
+            else
+            {
+                createDB();
+                connectDB();
+            }
+        }
+        // check db is exit if
+        public bool checkFilesExit()
+        {
+            return true;
+        }
+        public SQLiteConnection connectDB()
+        {
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=Config.db;Version=3;");
+            return m_dbConnection;
+
+        }
+        private void createDB()
+        {
+            SQLiteConnection.CreateFile("Config.db");
         }
     }
 }
